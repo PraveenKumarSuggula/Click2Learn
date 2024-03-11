f61cdc8e921c4487d065e3775daf28429e9a3519
PraveenSuggula <you@example.compraveenkumar.suggula0@gmail.com>
2024-03-11 15:09:04 -0500

    Instructor portal: Templete design 1

--- click2LearnInstructorClient/src/app/app-routing.module.ts
+++ click2LearnInstructorClient/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutUs', component: AboutComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'testimonial', component: TestimonialComponent },
+  { path: 'contactUs', component: ContactsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- click2LearnInstructorClient/src/app/app.module.ts
+++ click2LearnInstructorClient/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TeamComponent } from './team/team.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TeamComponent,
+    TestimonialComponent,
+    FooterComponent,
+    CategoriesComponent,
+    CoursesComponent,
+    CarouselComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactsComponent,
+    HomeComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
